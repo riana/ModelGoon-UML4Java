a655556c1d308a85fdb409be805029533df21952
riana <riana@cf35f469-cb7c-4352-bda5-e517d34bc848>
2011-04-05 22:52:26 +0000

    Communication diagram restored.
    git-svn-id: https://subversion.assembla.com/svn/ModelGoon/trunk@111 cf35f469-cb7c-4352-bda5-e517d34bc848

---
+++ org.modelgoon.plugin/src/org/modelgoon/jdt/communication/editor/JDTCommunicationDiagramEditor.java
@@ -0,0 +1,29 @@
+package org.modelgoon.jdt.communication.editor;
+
+import org.modelgoon.core.Note;
+import org.modelgoon.core.editparts.NoteEditPart;
+import org.modelgoon.jdt.communication.editParts.CommunicationClassEditPart;
+import org.modelgoon.jdt.communication.editParts.CommunicationEditPart;
+import org.modelgoon.jdt.editor.ClassModelElementFactory;
+import org.modelgoon.jdt.editor.JDTDiagramEditor;
+import org.modelgoon.jdt.editparts.JDTClassDiagramEditPart;
+import org.modelgoon.jdt.model.CommunicationRelationship;
+import org.modelgoon.jdt.model.UMLClass;
+import org.modelgoon.jdt.model.UMLModel;
+
+public class JDTCommunicationDiagramEditor extends JDTDiagramEditor {
+
+	public JDTCommunicationDiagramEditor() {
+		super(new UMLModel());
+		this.modelLoader
+				.addMapping("org/modelgoon/jdt/xml/CommunicationDiagram.cas");
+		setModelElementFactory(new ClassModelElementFactory(this));
+		registerEditPart(UMLModel.class, JDTClassDiagramEditPart.class);
+		registerEditPart(UMLClass.class, CommunicationClassEditPart.class);
+		registerEditPart(CommunicationRelationship.class,
+				CommunicationEditPart.class);
+
+		registerEditPart(Note.class, NoteEditPart.class);
+	}
+
+}
---
+++ org.modelgoon.plugin/src/org/modelgoon/jdt/communication/editor/NewCommunicationDiagramWizard.java
@@ -0,0 +1,75 @@
+package org.modelgoon.jdt.communication.editor;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard;
+
+public class NewCommunicationDiagramWizard extends BasicNewResourceWizard {
+
+	private WizardNewFileCreationPage mainPage;
+
+	/**
+	 * Creates a wizard for creating a new file resource in the workspace.
+	 */
+	public NewCommunicationDiagramWizard() {
+		super();
+	}
+
+	/*
+	 * (non-Javadoc) Method declared on IWizard.
+	 */
+	@Override
+	public void addPages() {
+		super.addPages();
+		this.mainPage = new WizardNewFileCreationPage("Default", getSelection());
+		this.mainPage.setTitle("New Interaction Diagram");
+		this.mainPage.setDescription("Creates a new Interaction Diagram");
+		this.mainPage.setFileExtension("mgi");
+		addPage(this.mainPage);
+	}
+
+	/*
+	 * (non-Javadoc) Method declared on IWorkbenchWizard.
+	 */
+	@Override
+	public void init(final IWorkbench workbench,
+			final IStructuredSelection currentSelection) {
+		super.init(workbench, currentSelection);
+		setWindowTitle("ModelGoon - New Interaction Diagram");
+		setNeedsProgressMonitor(true);
+	}
+
+	/*
+	 * (non-Javadoc) Method declared on IWizard.
+	 */
+	@Override
+	public boolean performFinish() {
+		IFile file = this.mainPage.createNewFile();
+		if (file == null) {
+			return false;
+		}
+
+		selectAndReveal(file);
+
+		// Open editor on new file.
+		IWorkbenchWindow dw = getWorkbench().getActiveWorkbenchWindow();
+		try {
+			if (dw != null) {
+				IWorkbenchPage page = dw.getActivePage();
+				if (page != null) {
+					IDE.openEditor(page, file, true);
+				}
+			}
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		}
+
+		return true;
+	}
+}
